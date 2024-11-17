0aee7a183c733e0e6df83fbf8a5c3e589c1a6f7f
Mubashir Ahmad <niazi9999mubashir@gmail.com>
2024-11-17 19:56:21 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function Item() {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id.Sed ut perspiciatis unde
             omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+            laudantium, totam rem aperiam, eaque ipsa ab illo inventore
             veritatis et quasi architecto beatae vitae dicta sunt explicabo.
           </p>
           <img
